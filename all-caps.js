f5ba8e08802e9f10cc705a15fab8433f0b16f077
xhmyjae <tomatis.mt@gmail.com>
2021-11-18 14:52:54 +0100

    all-caps

--- all-caps.js
+++ all-caps.js
@@ -1,2 +1,2 @@
-let noCaps = console.log(message.toUpperCase());
-let allCaps = console.log(message.toLowerCase());
+let noCaps = console.log(message.toLowerCase());
+let allCaps = console.log(message.toUpperCase());
