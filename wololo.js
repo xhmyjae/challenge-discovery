f3caa22fddd226902e45be91a4e2eddc2e5b5ffa
xhmyjae <tomatis.mt@gmail.com>
2021-11-18 16:26:07 +0100

    wololo

---
+++ wololo.js
@@ -0,0 +1,6 @@
+let stringFromNumber = String(num);
+let stringFromBoolean = String(bool);
+let numberFromString = Number(str);
+let numberFromBoolean = Number(bool);
+let booleanFromString = Boolean(str);
+let booleanFromNumber = Boolean(num);
