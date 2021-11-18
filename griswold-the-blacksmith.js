0d5a27b64913f5ae0e715ed763146ade642b978f
xhmyjae <tomatis.mt@gmail.com>
2021-11-18 17:00:03 +0100

    grisworld-the-blacksmith

---
+++ griswold-the-blacksmith.js
@@ -0,0 +1,10 @@
+let purchasableGoods = [];
+if (playerCoins >= 3) {
+    purchasableGoods.push("arrows");
+}
+if (playerCoins >= 44) {
+    purchasableGoods.push("boots");
+}
+if (playerCoins >= 299.99) {
+    purchasableGoods.push("sword");
+}
