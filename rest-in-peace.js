83a854a6d86db91358b2708388568008b2a282e9
xhmyjae <tomatis.mt@gmail.com>
2021-11-18 16:43:04 +0100

    rest-in-peace

--- rest-in-peace.js
+++ rest-in-peace.js
@@ -1,3 +1,3 @@
 let footSoldiers = [...swordMen, ...archers, "Grim the Terrible"];
 let horseMen = ["The Skull Knight", ...captains, ...generals];
-let armyOfTheDead = [...footSoldiers, ...horseMen, "The King of the Dead"];
+let armyOfTheDead = [...footSoldiers, "The King of the Dead", ...horseMen];
