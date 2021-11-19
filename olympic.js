fae470a9993e0dbb8d43cb5d590f3c8fcc08c4c4
xhmyjae <tomatis.mt@gmail.com>
2021-11-19 23:49:03 +0100

    olympic

---
+++ olympic.js
@@ -0,0 +1,9 @@
+if (athletes[0] === "Pedro") {
+    console.log("🥇");
+} else if (athletes[1] === "Pedro") {
+    console.log("🥈");
+} else if (athletes[2] === "Pedro") {
+    console.log("🥉");
+} else {
+    console.log("😞");
+}
