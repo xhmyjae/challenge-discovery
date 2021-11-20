7a79fa204bce27b42efdab576bb6bed1b70724e9
xhmyjae <tomatis.mt@gmail.com>
2021-11-20 21:57:49 +0100

    happy

--- happy.js
+++ happy.js
@@ -1,10 +1,9 @@
 function happy(arg) {
     let res;
-    let lowarg = arg.toLowerCase()
-    if (lowarg.includes("happy") && lowarg.includes("?") && lowarg[lowarg.length-1] === "?") {
-        res = true;
-    } else {
-        res = false;
-    }
+    let lowarg = arg.toLowerCase();
+    lowarg.includes("happy") ? res = true : res = false;
+    lowarg.includes("?") ? res = true : res = false;
+    lowarg[lowarg.length-1] === "?" ? res = true : res = false;
+
     console.log(res)
 }
