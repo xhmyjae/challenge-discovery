2fc6a956965e8dab3a1688354d8d3ad60758aa16
xhmyjae <tomatis.mt@gmail.com>
2021-11-20 21:36:32 +0100

    level-up

---
+++ level-up.js
@@ -0,0 +1,7 @@
+function shaker(quantity, fruit, alcohol) {
+    if (alcohol) {
+        console.log(quantity, fruit, "cocktail");
+    } else {
+        console.log(quantity, fruit, "milkshake");
+    }
+}
