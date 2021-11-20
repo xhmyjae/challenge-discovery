41d8aaaae5a0106ba5aa60416e56395cc86ca986
xhmyjae <tomatis.mt@gmail.com>
2021-11-20 21:19:34 +0100

    two-can-play-that-game

---
+++ two-can-play-that-game.js
@@ -0,0 +1,7 @@
+function duos(name, name2) {
+    console.log(name, " and ", name2, " !");
+}
+
+function duosWork(name, name2, action) {
+    console.log(name, " and ", name2, " ", action, " !");
+}
