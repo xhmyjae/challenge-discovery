ddc7816c046329052a75292b68d95664ae29778a
xhmyjae <tomatis.mt@gmail.com>
2021-11-22 22:07:00 +0100

    action-reaction

--- action-reaction.js
+++ action-reaction.js
@@ -2,19 +2,16 @@ let eye = document.getElementById("eye-left");
 let close = false;
 const btn = document.querySelector("button");
 
-window.onload = function() {
-
-    btn.onclick = function () {
-        if (close) {
-            eye.style.backgroundColor = "red";
-            btn.innerHTML = "Click to open the left eye";
-            close = true;
-            eye.classList.remove("eye-closed");
-        } else {
-            eye.style.backgroundColor = "black";
-            btn.innerHTML = "Click to close the left eye";
-            close = false;
-            eye.classList.add("eye-closed");
-        }
+btn.onclick = function() {
+    if (close) {
+        eye.style.backgroundColor = "red";
+        btn.innerHTML = "Click to open the left eye";
+        close = true;
+        eye.classList.remove("eye-closed");
+    } else {
+        eye.style.backgroundColor = "black";
+        btn.innerHTML = "Click to close the left eye";
+        close = false;
+        eye.classList.add("eye-closed");
     }
-}
+};
